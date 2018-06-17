974c53b7e23b3a104d2edbd5e0d4d3cfb585382c
Gibby <john.gibby@localmed.com>
2018-06-17 15:54:19 -0500

    lesson complete

--- server/main.js
+++ server/main.js
@@ -1,5 +1,26 @@
 import { Meteor } from 'meteor/meteor';
 
+const today = new Date();
+
 Meteor.startup(() => {
   // code to run on server at startup
+
+  if (Blog.find().count() < 1) {
+    Blog.insert({
+      title: 'Test Entry',
+      slug: 'test-entry',
+      introText: 'Just an early test entry, will be deleted later',
+      blogImage: 'https://d14jjfgstdxsoz.cloudfront.net/assets/logo.svg',
+      datePosted: new Date(),
+      content: 'Just some more content for our test entry. Not much to show here yet.',
+      categories: ['test category'],
+    });
+  }
+
+  if (Categories.find().count() < 1) {
+    Categories.insert({
+      category: 'test category',
+      slug: 'test-category',
+    });
+  }
 });
