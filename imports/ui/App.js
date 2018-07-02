3e675fc5d2be5fe5689d16a4877eaebf55a49f72
Gibby <john.gibby@localmed.com>
2018-07-02 07:29:28 -0500

    updating app.js

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -3,12 +3,49 @@ import React, { Component } from 'react';
 export default class App extends Component {
 
   render () {
-    console.log(this.props);
+    const {from, blog, categories} = this.props;
     return (
       <div className="app-container">
-        <header>Meteor SSR Blog Tutorial {this.props.from}</header>
+        {this._renderHeader(from)}
+        {this._renderMain(blog, categories)}
       </div>
     );
+  }
+
+  _renderHeader (from) {
+    return (
+      <header>
+        <div className="units-container">
+          <h1>Meteor SSR Blog Tutorial ({from})</h1>
+          <nav className="navbar">
+            <ul>
+              <li><a href="" title="Archives">Archives</a></li>
+              <li><a href="" title="Categories">Categories</a></li>
+            </ul>
+          </nav>
+        </div>
+      </header>
+    )
+  }
+
+  _renderMain (blog, categories) {
+    if (blog) {
+      return (
+        <main className="units-container units-row">
+          <article className="unit-66">
+            <h2>{blog.title}</h2>
+            <figure>
+              <img src={blog.blogImage} alt={blog.title} />
+            </figure>
+            <p><strong>{blog.introText}</strong></p>
+            <a href={`/blog/${blog.slug}}`} className="btn btn-blue"></a>
+          </article>
+          <aside className="unit-33">
+
+          </aside>
+        </main>
+      );
+    }
   }
 
 }
