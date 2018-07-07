d6fe0af7a04d8134254274a4ac31cd007f1cd713
Gibby <john.gibby@localmed.com>
2018-07-07 11:36:49 -0500

    basic wireframe

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -29,22 +29,49 @@ export default class App extends Component {
   }
 
   _renderMain (blog, categories) {
-    if (blog) {
+    if (blog && categories) {
       return (
-        <main className="units-container units-row">
-          <article className="unit-66">
-            <h2>{blog.title}</h2>
-            <figure>
-              <img src={blog.blogImage} alt={blog.title} />
-            </figure>
-            <p><strong>{blog.introText}</strong></p>
-            <a href={`/blog/${blog.slug}}`} className="btn btn-blue"></a>
-          </article>
-          <aside className="unit-33">
+        <main className="units-container">
+          <div className="units-row">
+            <article className="unit-66">
+              <a href={`/blog/${blog.slug}`} className="landing-blog-snippet">
+                <h2>{blog.title}</h2>
+                <figure>
+                  <img src={blog.blogImage} alt={blog.title} />
+                </figure>
+                <p><strong>{blog.introText}</strong></p>
+              </a>
+              <a href={`/blog/${blog.slug}}`} className="btn btn-blue">Read More</a>
 
-          </aside>
+              <div className="landing-blog-categories">
+                <h4>This Post Covers: </h4>
+                <div className="btn-group width-100">
+                  {this._renderCategories(categories)}
+                </div>
+              </div>
+            </article>
+            <div className="unit-33">
+              <aside>
+                <h3>View Other Posts</h3>
+              </aside>
+            </div>
+          </div>
         </main>
       );
+    }
+  }
+
+  _renderCategories (categories) {
+    if (categories) {
+      const formattedCategories = categories.map((cat, ii) => {
+        return (
+          <a href={`/blog/categories/${cat.slug}`}
+             className="btn btn-outline btn-black"
+             key={ii}>{cat.category}</a>
+        )
+      });
+
+      return formattedCategories;
     }
   }
 
